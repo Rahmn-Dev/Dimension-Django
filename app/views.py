a58547db8ac451bbec26dbb8a7886ab8ac4ad1b8
Rahmn-Dev <sidqi.rahman@gmail.com>
2024-01-06 09:58:57 +0000

    updating Templates

--- app/views.py
+++ app/views.py
@@ -6,7 +6,11 @@ from django.contrib.auth.decorators import login_required
 
 @login_required
 def dashboard(request):
-    return render(request, 'dashboard.html')
+    items = Item.objects.all()
+    containers = Container.objects.all()
+    trucks = Truck.objects.all()
+    
+    return render(request, 'dashboard.html', {'items': items, 'containers': containers, 'trucks': trucks})
 
 @login_required
 def item_form(request):
